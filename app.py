a3a3e1f62fe6ce877c197dbde3bf41376a766037
pixls <tha.pixls@gmail.com>
2022-03-02 14:54:26 -0500

    added black formatter
    formatted starting documents:

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask
 from models import db, connect_db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
 connect_db(app)
 db.create_all()
