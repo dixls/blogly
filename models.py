18cfca4200baffa7729b05880ab8a44d147553a6
pixls <tha.pixls@gmail.com>
2022-03-04 13:21:34 -0500

    made posts table
    created class in models for posts table, not sure if datetime will work right

--- models.py
+++ models.py
@@ -19,3 +19,13 @@ class User(db.Model):
     first_name = db.Column(db.String(25), nullable=False)
     last_name = db.Column(db.String(40), nullable=False)
     img_url = db.Column(db.String(191))
+
+class Post(db.Model):
+    """Posts table"""
+
+    __tablename__= "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
