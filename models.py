4f078d140a7f7a77546a8729108dbc2b72de72f6
pixls <tha.pixls@gmail.com>
2022-03-08 13:43:46 -0500

    created Tag and PostTag models
    Tag model for tags to apply to posts, PostTag for a many to many relationship from posts to tags

--- models.py
+++ models.py
@@ -23,18 +23,39 @@ class User(db.Model):
     def __repr__(self):
         return f"<USER name = {self.first_name} {self.last_name}, ID = {self.id}>"
 
+
 class Post(db.Model):
     """Posts table"""
 
-    __tablename__= "posts"
+    __tablename__ = "posts"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
-    author = db.relationship('User', backref='posts')
+    author = db.relationship("User", backref="posts")
+
+    tags = db.relationship("Tag", secondary="post_tags", backref="posts")
 
     def __repr__(self):
         return f"<post Title = {self.title}, AUTHOR = {self.author.first_name} {self.author.last_name}>"
+
+
+class Tag(db.Model):
+    """tags for a given post"""
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+
+class PostTag(db.Model):
+    """relationships of tags to posts"""
+
+    __tablename__ = "posts_tags"
+
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key=True)
